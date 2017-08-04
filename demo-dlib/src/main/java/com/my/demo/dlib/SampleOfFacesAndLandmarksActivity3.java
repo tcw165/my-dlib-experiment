0df27bcfa7c500b60f68363f9eab0fff543c86d2
Boy Wang <boyw165@gmail.com>
2017-08-04 10:31:07 +0800

    Use Camera2 API in SampleOfFacesAndLandmarksActivity3.java.

--- demo-dlib/src/main/java/com/my/demo/dlib/SampleOfFacesAndLandmarksActivity3.java
+++ demo-dlib/src/main/java/com/my/demo/dlib/SampleOfFacesAndLandmarksActivity3.java
@@ -22,20 +22,43 @@ package com.my.demo.dlib;
 
 import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.ImageFormat;
+import android.graphics.Matrix;
+import android.graphics.RectF;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.ImageReader;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
 import android.view.View;
 
 import com.my.core.protocol.IProgressBarView;
-import com.my.demo.dlib.protocol.ICameraMetadata;
 import com.my.demo.dlib.view.AutoFitTextureView;
 import com.my.demo.dlib.view.FaceLandmarksOverlayView;
 import com.my.demo.dlib.view.TextureViewObservable;
 import com.my.jni.dlib.DLibLandmarks68Detector;
 import com.my.jni.dlib.IDLibFaceDetector;
 import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -48,8 +71,7 @@ import io.reactivex.functions.Function;
 
 public class SampleOfFacesAndLandmarksActivity3
     extends AppCompatActivity
-    implements ICameraMetadata,
-               IProgressBarView {
+    implements IProgressBarView {
 
     // View.
     @BindView(R.id.texture)
@@ -60,6 +82,17 @@ public class SampleOfFacesAndLandmarksActivity3
 
     // Butter Knife.
     Unbinder mUnbinder;
+
+    // Camera configuration.
+    int mSensorOrientation;
+    Size mVideoSize;
+    Size mPreviewSize;
+    CameraManager mCameraManager;
+    private CameraDevice mCameraDevice;
+    private CameraCaptureSession mCaptureSession;
+    private ImageReader mPreviewImageReader;
+    private HandlerThread mBackgroundThread;
+    private Handler mBackgroundHandler;
 
     // DLibFace Detector.
     IDLibFaceDetector mLandmarksDetector;
@@ -87,6 +120,11 @@ public class SampleOfFacesAndLandmarksActivity3
     protected void onResume() {
         super.onResume();
 
+        // Starts a background thread
+        mBackgroundThread = new HandlerThread("CameraBackground");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+
         // TODO: Check if the Google Play Service is present.
         mDisposables = new CompositeDisposable();
         mDisposables.add(
@@ -113,6 +151,12 @@ public class SampleOfFacesAndLandmarksActivity3
                     @Override
                     public void accept(Object anything)
                         throws Exception {
+                        // Configure the camera.
+                        final String cameraId = configureCamera();
+
+                        // TODO: Start the camera preview.
+                        // Open camera and start the capture session.
+                        openCamera(cameraId);
                         Log.d("xyz", anything.toString());
                     }
                 }));
@@ -122,31 +166,20 @@ public class SampleOfFacesAndLandmarksActivity3
     protected void onPause() {
         super.onPause();
 
+        // Stops the background thread
+        mBackgroundThread.quitSafely();
+        try {
+            mBackgroundThread.join();
+            mBackgroundThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Force to hide the progress bar.
         hideProgressBar();
 
         mDisposables.clear();
-    }
-
-    @Override
-    public boolean isFacingFront() {
-        return false;
-    }
-
-    @Override
-    public boolean isFacingBack() {
-        return false;
-    }
-
-    @Override
-    public boolean isPortraitMode() {
-        int orientation = getResources().getConfiguration().orientation;
-        return orientation == Configuration.ORIENTATION_PORTRAIT;
-    }
-
-    @Override
-    public boolean isLandscapeMode() {
-        int orientation = getResources().getConfiguration().orientation;
-        return orientation == Configuration.ORIENTATION_LANDSCAPE;
     }
 
     @Override
@@ -202,6 +235,267 @@ public class SampleOfFacesAndLandmarksActivity3
                      Manifest.permission.CAMERA);
     }
 
+    /**
+     * In this sample, we choose a video size with 3x4 aspect ratio. Also, we don't use sizes
+     * larger than 1080p, since MediaRecorder cannot handle such a high-resolution video.
+     *
+     * @param choices The list of available sizes
+     * @return The video size
+     */
+    private Size chooseVideoSize(Size[] choices) {
+        for (Size size : choices) {
+            if (size.getWidth() == size.getHeight() * 4 / 3 && size.getWidth() <= 1080) {
+                return size;
+            }
+        }
+        Log.e("xyz", "Couldn't find any suitable video size");
+        return choices[choices.length - 1];
+    }
+
+    /**
+     * Given {@code choices} of {@code Size}s supported by a camera, chooses the smallest one whose
+     * width and height are at least as large as the respective requested values, and whose aspect
+     * ratio matches with the specified value.
+     *
+     * @param choices     The list of sizes that the camera supports for the intended output class
+     * @param width       The minimum desired width
+     * @param height      The minimum desired height
+     * @param aspectRatio The aspect ratio
+     * @return The optimal {@code Size}, or an arbitrary one if none were big enough
+     */
+    private Size chooseOptimalSize(Size[] choices,
+                                   int width,
+                                   int height,
+                                   Size aspectRatio) {
+        // Collect the supported resolutions that are at least as big as the preview Surface
+        List<Size> bigEnough = new ArrayList<>();
+        int w = aspectRatio.getWidth();
+        int h = aspectRatio.getHeight();
+        for (Size option : choices) {
+            if (option.getHeight() == option.getWidth() * h / w &&
+                option.getWidth() >= width && option.getHeight() >= height) {
+                bigEnough.add(option);
+            }
+        }
+
+        // Pick the smallest of those, assuming we found any
+        if (bigEnough.size() > 0) {
+            return Collections.min(bigEnough, new CompareSizesByArea());
+        } else {
+            Log.e("xyz", "Couldn't find any suitable preview size");
+            return choices[0];
+        }
+    }
+
+    /**
+     * Configures the necessary {@link android.graphics.Matrix} transformation to `mTextureView`.
+     * This method should not to be called until the camera preview size is determined in
+     * openCamera, or until the size of `mTextureView` is fixed.
+     *
+     * @param viewWidth  The width of `mTextureView`
+     * @param viewHeight The height of `mTextureView`
+     */
+    private void configureTransform(int viewWidth, int viewHeight) {
+        if (null == mCameraView || null == mPreviewSize) {
+            return;
+        }
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+        Matrix matrix = new Matrix();
+        RectF viewRect = new RectF(0, 0, viewWidth, viewHeight);
+        RectF bufferRect = new RectF(0, 0, mPreviewSize.getHeight(), mPreviewSize.getWidth());
+        float centerX = viewRect.centerX();
+        float centerY = viewRect.centerY();
+        if (Surface.ROTATION_90 == rotation || Surface.ROTATION_270 == rotation) {
+            bufferRect.offset(centerX - bufferRect.centerX(), centerY - bufferRect.centerY());
+            matrix.setRectToRect(viewRect, bufferRect, Matrix.ScaleToFit.FILL);
+            float scale = Math.max(
+                (float) viewHeight / mPreviewSize.getHeight(),
+                (float) viewWidth / mPreviewSize.getWidth());
+            matrix.postScale(scale, scale, centerX, centerY);
+            matrix.postRotate(90 * (rotation - 2), centerX, centerY);
+        }
+        mCameraView.setTransform(matrix);
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private String configureCamera() throws CameraAccessException,
+                                            NullPointerException {
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        String cameraId = mCameraManager.getCameraIdList()[0];
+
+        // Choose the sizes for camera preview and video recording
+        CameraCharacteristics characteristics = mCameraManager.getCameraCharacteristics(cameraId);
+        StreamConfigurationMap map = characteristics
+            .get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+        if (map == null) {
+            throw new RuntimeException("Cannot get available preview/video sizes");
+        }
+        mSensorOrientation = characteristics.get(CameraCharacteristics.SENSOR_ORIENTATION);
+        // TODO: Why the video size is different from preview size?
+        mVideoSize = chooseVideoSize(map.getOutputSizes(MediaRecorder.class));
+        mPreviewSize = chooseOptimalSize(map.getOutputSizes(SurfaceTexture.class),
+                                         mCameraView.getWidth(),
+                                         mCameraView.getHeight(),
+                                         mVideoSize);
+
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            mCameraView.setAspectRatio(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+        } else {
+            mCameraView.setAspectRatio(mPreviewSize.getHeight(), mPreviewSize.getWidth());
+        }
+        configureTransform(mPreviewSize.getWidth(),
+                           mPreviewSize.getHeight());
+
+        return cameraId;
+    }
+
+    /**
+     * Tries to open a {@link CameraDevice}. The result is listened by `mStateCallback`.
+     */
+    @SuppressWarnings("MissingPermission")
+    private void openCamera(String cameraId) throws CameraAccessException {
+        mCameraManager.openCamera(
+            cameraId,
+            new CameraDevice.StateCallback() {
+
+                @Override
+                public void onOpened(@NonNull CameraDevice cameraDevice) {
+                    mCameraDevice = cameraDevice;
+                    createCaptureSession();
+//            mCameraOpenCloseLock.release();
+//            if (null != mTextureView) {
+//                configureTransform(mTextureView.getWidth(), mTextureView.getHeight());
+//            }
+                }
+
+                @Override
+                public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+//            mCameraOpenCloseLock.release();
+                    cameraDevice.close();
+                    mCameraDevice = null;
+                }
+
+                @Override
+                public void onError(@NonNull CameraDevice cameraDevice, int error) {
+//            mCameraOpenCloseLock.release();
+                    cameraDevice.close();
+                    mCameraDevice = null;
+//            Activity activity = getActivity();
+//            if (null != activity) {
+//                activity.finish();
+//            }
+                }
+            },
+            null);
+    }
+
+    /**
+     * Start the camera preview.
+     */
+    private void createCaptureSession() {
+        if (null == mCameraDevice || !mCameraView.isAvailable() || null == mPreviewSize) {
+            return;
+        }
+        try {
+            closeCaptureSession();
+
+            // Prepare surfaces for capture session.
+            final List<Surface> surfaces = new ArrayList<>();
+
+            // Configure the preview.
+            SurfaceTexture texture = mCameraView.getSurfaceTexture();
+            texture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+
+            final CaptureRequest.Builder requestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+
+            Surface previewSurface = new Surface(texture);
+            requestBuilder.addTarget(previewSurface);
+
+            surfaces.add(previewSurface);
+
+            // Configure the reader for the preview frames.
+            mPreviewImageReader =
+                ImageReader.newInstance(
+                    mPreviewSize.getWidth(), mPreviewSize.getHeight(),
+                    ImageFormat.YUV_420_888, 2);
+            mPreviewImageReader.setOnImageAvailableListener(mOnImageAvailableListener,
+                                                            mBackgroundHandler);
+
+            surfaces.add(mPreviewImageReader.getSurface());
+
+            // Create capture session.
+            mCameraDevice.createCaptureSession(
+                Collections.singletonList(previewSurface),
+                new CameraCaptureSession.StateCallback() {
+
+                    @Override
+                    public void onConfigured(@NonNull CameraCaptureSession session) {
+                        // The camera is already closed
+                        if (null == mCameraDevice) {
+                            return;
+                        }
+
+                        // When the session is ready, we start displaying the preview.
+                        mCaptureSession = session;
+
+                        try {
+                            // Auto focus should be continuous for camera preview.
+                            requestBuilder.set(
+                                CaptureRequest.CONTROL_AF_MODE,
+                                CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+                            // Flash is automatically enabled when necessary.
+                            requestBuilder.set(
+                                CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
+
+                            // Finally, we start displaying the camera preview.
+                            CaptureRequest previewRequest = requestBuilder.build();
+                            mCaptureSession.setRepeatingRequest(
+                                previewRequest, null, mBackgroundHandler);
+                        } catch (final CameraAccessException e) {
+                            // IGNORED.
+                        }
+                    }
+
+                    @Override
+                    public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                        // IGNORED.
+                    }
+                }, mBackgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeCaptureSession() {
+        if (mCaptureSession != null) {
+            mCaptureSession.close();
+            mCaptureSession = null;
+        }
+    }
+
+    private ImageReader.OnImageAvailableListener mOnImageAvailableListener =
+        new ImageReader.OnImageAvailableListener() {
+            @Override
+            public void onImageAvailable(ImageReader reader) {
+                Log.d("xyz", "Read image!");
+            }
+        };
+
     ///////////////////////////////////////////////////////////////////////////
     // Clazz //////////////////////////////////////////////////////////////////
+
+    /**
+     * Compares two {@code Size}s based on their areas.
+     */
+    private static class CompareSizesByArea implements Comparator<Size> {
+
+        @Override
+        public int compare(Size lhs, Size rhs) {
+            // We cast here to ensure the multiplications won't overflow
+            return Long.signum((long) lhs.getWidth() * lhs.getHeight() -
+                               (long) rhs.getWidth() * rhs.getHeight());
+        }
+    }
 }
